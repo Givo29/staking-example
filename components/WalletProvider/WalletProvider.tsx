fad3d9c12c38106544dd458faf742be3d7ce4236
abrarmusa <abrar.musa.89@gmail.com>
2022-05-09 05:04:19 +0600

    a

--- components/WalletProvider/WalletProvider.tsx
+++ components/WalletProvider/WalletProvider.tsx
@@ -23,15 +23,15 @@ import { clusterApiUrl } from "@solana/web3.js"
 // Default styles that can be overridden by your app
 require("@solana/wallet-adapter-react-ui/styles.css")
 
-const Wallet: FC = ({ children }) => {
+const Wallet: FC = ({ children }: { children: React.ReactChild }) => {
   // The network can be set to 'devnet', 'testnet', or 'mainnet-beta'.
   const network = process.env
     .NEXT_PUBLIC_CONNECTION_NETWORK as WalletAdapterNetwork
 
   const endpoint =
-    process.env.NEXT_PUBLIC_CONNECTION_NETWORK! == "devnet"
-      ? process.env.NEXT_PUBLIC_SOLANA_RPC_HOST_DEVNET!
-      : process.env.NEXT_PUBLIC_SOLANA_RPC_HOST_MAINNET_BETA!
+    process.env.NEXT_PUBLIC_CONNECTION_NETWORK == "devnet"
+      ? process.env.NEXT_PUBLIC_SOLANA_RPC_HOST_DEVNET
+      : process.env.NEXT_PUBLIC_SOLANA_RPC_HOST_MAINNET_BETA
 
   // @solana/wallet-adapter-wallets includes all the adapters but supports tree shaking and lazy loading --
   // Only the wallets you configure here will be compiled into your application, and only the dependencies
